a731a83e1b18186c89b5ce356904cac43756d44f
Chris Collis <collisdigital@users.noreply.github.com>
2026-02-05 23:50:37 +0000

    Fix Python Debugging Support in VSCode

--- docker/entrypoint.app.dev.sh
+++ docker/entrypoint.app.dev.sh
@@ -2,7 +2,7 @@
 set -e
 
 if [ "$DEBUG" = "true" ]; then
-  exec uv run --with debugpy python -m debugpy --listen 0.0.0.0:5678 --multiprocess -m uvicorn main:app --host 0.0.0.0 --port 8000 --loop uvloop --reload
+  exec uv run --with debugpy python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:5678 -m uvicorn main:app --host 0.0.0.0 --port 8000 --loop uvloop --reload
 else
   exec uv run uvicorn main:app --host 0.0.0.0 --port 8000 --loop uvloop --reload
 fi
